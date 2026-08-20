d2717557b4f71bd8e76779414f7ca13fb96d095c
rrr89817384657-collab <rrr89817384657@gmail.com>
2026-08-20 16:29:55 +0300

    Create variable.tf

---
+++ files/terraform/variable.tf
@@ -0,0 +1,13 @@
+variable "yandex_cloud_id" {
+    default = "b1gnfsvvugjvrkpvs34a"
+  
+}
+
+variable "yandex_folder_id" {
+    default = "b1gdtdsm3gtnu5k218d7"
+  
+}
+
+variable "yandex_default_zone" {
+    default = "ru-central1-a"
+}
